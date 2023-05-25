07072a1718b2168cbf5cb5894caba22f66b92de3
For Glory <forglory0905@outlook.com>
2023-05-25 12:20:30 -0700

    integrate google oauth

--- template-reactless/my_component/frontend/src/index.tsx
+++ template-reactless/my_component/frontend/src/index.tsx
@@ -4,25 +4,16 @@ import { Streamlit, RenderData } from "streamlit-component-lib"
 // to index.html.)
 const span = document.body.appendChild(document.createElement("span"))
 const textNode = span.appendChild(document.createTextNode(""))
-const button = span.appendChild(document.createElement("button"))
-button.textContent = "Click Me!"
+const txtToken = span.appendChild(document.createElement("input"))
+txtToken.id = "txtToken"
+
+
 
 // Add a click handler to our button. It will send data back to Streamlit.
-let numClicks = 0
-let isFocused = false
-button.onclick = function(): void {
+txtToken.onchange = function(e: any): void {
   // Increment numClicks, and pass the new value back to
   // Streamlit via `Streamlit.setComponentValue`.
-  numClicks += 1
-  Streamlit.setComponentValue(numClicks)
-}
-
-button.onfocus = function(): void {
-  isFocused = true
-}
-
-button.onblur = function(): void {
-  isFocused = false
+  Streamlit.setComponentValue(e.target.value)
 }
 
 /**
@@ -34,20 +25,8 @@ function onRender(event: Event): void {
   // Get the RenderData from the event
   const data = (event as CustomEvent<RenderData>).detail
 
-  // Maintain compatibility with older versions of Streamlit that don't send
-  // a theme object.
-  if (data.theme) {
-    // Use CSS vars to style our button border. Alternatively, the theme style
-    // is defined in the data.theme object.
-    const borderStyling = `1px solid var(${
-      isFocused ? "--primary-color" : "gray"
-    })`
-    button.style.border = borderStyling
-    button.style.outline = borderStyling
-  }
-
   // Disable our button if necessary.
-  button.disabled = data.disabled
+  txtToken.disabled = data.disabled
 
   // RenderData.args is the JSON dictionary of arguments sent from the
   // Python script.
@@ -60,7 +39,7 @@ function onRender(event: Event): void {
   // case it has changed. (This isn't strictly necessary for the example
   // because our height stays fixed, but this is a low-cost function, so
   // there's no harm in doing it redundantly.)
-  Streamlit.setFrameHeight()
+  Streamlit.setFrameHeight(100)
 }
 
 // Attach our `onRender` handler to Streamlit's render event.
